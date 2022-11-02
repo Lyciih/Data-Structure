db6624f650b6e843a183bd741787a1715ac8eafb
Lyciih <gnehciyil@gmail.com>
2022-11-02 20:12:56 +0800

    linked list

---
+++ Linked List/src/DLL_next_node.c
@@ -0,0 +1,11 @@
+#include "linked_list.h"
+
+dllNode_t * DLL_next_node(const dllNode_t * node)//(下一個節點)
+{
+	return node->next;
+}
+
+dllNode_t * DLL_prev_node(const dllNode_t * node)//(上一個節點)
+{
+	return node->prev;
+}
