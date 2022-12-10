62773620aa69dfa85a2a27fe6b4b745e70ccc2c2
Lyciih <gnehciyil@gmail.com>
2022-12-10 18:09:58 +0800

    pq

--- priority queue/test.c
+++ priority queue/test.c
@@ -48,24 +48,34 @@ int main() {
     printf("MINIMUM HEAP :\n");
 
 
-    student_t node[6] = {
+    student_t node[10] = {
         {"A", 70, 100},
         {"B", 60, 90},
         {"C", 80, 95},
         {"D", 65, 90},
         {"E", 10, 70},
-        {"F", 90, 90}
+        {"F", 90, 90},
+        {"G", 20, 60},
+        {"H", 30, 50},
+        {"I", 40, 40},
+        {"J", 50, 30}
     };
 
     PQ_t first_pq;
     createPQ(&first_pq, MINHEAP, sizeof(student_t), 10, compareMath);
 
-    for(int i = 0 ; i < 6 ; i++)
+    printf("is queue empty? 0->true 1->false : %d\n", IsEmpty(&first_pq));
+
+
+
+    for(int i = 0 ; i < 10 ; i++)
     {
 	    Enqueue(&first_pq, &node[i]);
         pq_printf_tree(&first_pq, get_math);
     }
 
+    printf("\n");
+    printf("is queue full? 0->true 1->false : %d\n", IsFull(&first_pq));
     printf("\n");
     printf("DELETE :\n");
 
@@ -82,20 +92,24 @@ int main() {
     printf("MAXIMUM HEAP :\n");
 
 
-    student_t node_two[6] = {
+    student_t node_two[10] = {
         {"A", 70, 100},
         {"B", 60, 90},
         {"C", 80, 95},
         {"D", 65, 90},
         {"E", 10, 70},
-        {"F", 90, 90}
+        {"F", 90, 90},
+        {"G", 20, 60},
+        {"H", 30, 50},
+        {"I", 40, 40},
+        {"J", 50, 30}
     };
 
 
     PQ_t second_pq;
     createPQ(&second_pq, MAXHEAP, sizeof(student_t), 10, compareMath);
 
-    for(int i = 0 ; i < 6 ; i++)
+    for(int i = 0 ; i < 10 ; i++)
     {
 	    Enqueue(&second_pq, &node_two[i]);
         pq_printf_tree(&second_pq, get_math);
